ecdfd40aff88ff9d26a99d050c1806150d4b98b0
carlsus <carlsus@gmail.com>
2023-04-09 19:21:34 +0800

    EasterSunday Update

--- resources/views/employeesaccountability/index.blade.php
+++ resources/views/employeesaccountability/index.blade.php
@@ -8,7 +8,7 @@
       <div class="col-12">
         <div class="card">
           <div class="card-header">
-            <h3 class="card-title">Employee Accountability</h3>
+            <h3 class="card-title">Equipment Management</h3>
           </div>
           <!-- /.card-header -->
           <div class="card-body">
